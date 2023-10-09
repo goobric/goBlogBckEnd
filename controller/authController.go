1e7167172ed92a3e2b8e741249c596789a47f329
mlaane <goobric@gmail.com>
2023-10-09 15:57:06 +0100

    auth0

--- controller/authController.go
+++ controller/authController.go
@@ -1,1 +1,75 @@
 package controller
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"strings"
+
+	"github.com/altonymous/forum/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+func validateEmail(email string) bool {
+	Re := regexp.MustCompile(`[a-z0-9._%+\-]+@[a-z0-9._%+\-]+\.[a-z0-9._%+\-]`)
+	return Re.MatchString(email)
+}
+
+func Register(c *fiber.Ctx) error {
+	var data map[string]interface{}
+	var userData models.User
+	if err := c.BodyParser(&data); err != nil {
+		fmt.Println("Unable to parse body")
+	}
+	// Check if password is less than 6 character
+	if len(data["password"].(string)) <= 6 {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "Password must be greater that 6 characters",
+		})
+	}
+	if !validateEmail(strings.TrimSpace(data["email"].(string))) {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "Invalid email address",
+		})
+	}
+	//Check if password already exists in database
+	database.DB.Where("email=?", strings.TrimSpace(data["email"].(string))).First(&userData)
+	if userData.Id != 0 {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "Email already exist",
+		})
+	}
+	user := models.User{
+		FirstName: data["first_name"].(string),
+		LastName:  data["last_name"].(string),
+		Email:     strings.TrimSpace(data["email"].(string)),
+	}
+	user.SetPassword(data["password"].(string))
+	err != database.DB.Create(&user)
+	if err != nil {
+		log.Println(err)
+	}
+	c.Status(200)
+	return c.JSON(fiber.Map{
+		"user":    user,
+		"message": "Account created successfully",
+	})
+}
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+	if err := c.BodyParser(&data); err != nil {
+		fmt.Println("Unable to parse body")
+	}
+	var user models.User
+	database.DB.Where("email=?", data["email"]).First(&user)
+	if user.Id == 0 {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "Email does not exist, please create an account",
+		})
+	}
+}
